d609889199a05846340688b227a8981ff05390a3
Ines Akrap <ines.akrap@netcentric.biz>
2020-11-05 11:50:10 +0100

    Project setup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ProgUX Demo Site
 
+## Pages
+If you add new pages, update js/routes.js
+
+Pages are loaded via the page query parameter. So https://vanillaHTML.com/?page=terms will load the terms.html page. This is setup in routes.js
+
+## Components
+Web components can be built as single HTML files. CSS at the top, HTML in the middle and Javascript at the bottom. Include all Javascript between script tags at the bottom of the file, don't use inline Javascript.
+
+See the examples in components/
+
+These can be loaded via the utils.loadModule('components/header.html','header');
+
+1st parameter is the path to the file, 2nd is the dom ID to inject the code.
+
+This allows for modular and 3rd party components to be added with ease.
+
+
---
+++ html/js/routes.js
@@ -0,0 +1,38 @@
+import { utils } from './../core/utils.js';
+/* global vSettings */
+
+export const routes = {
+	init: async () => {
+		const get = utils.getQueryParams();
+		if (get.page && get.page === 'home') {
+			// Home Page
+			await utils.loadModule('pages/home.html','content');
+			utils.setTitle(`${vSettings.brand} | Home`);
+		} else if (get.page && get.page === 'demo') {
+			// Demo
+			await utils.loadModule('pages/demo.html','content', (pageContent) => {
+				return pageContent.split('[brand]').join(vSettings.brand);
+			});
+			utils.setTitle(`${vSettings.brand} Demo`);
+		} else if (get.page && get.page === 'documentation') {
+			// Documentation
+			await utils.loadModule('pages/documentation.html','content', (pageContent) => {
+				return pageContent.split('[brand]').join(vSettings.brand);
+			});
+			utils.setTitle(`${vSettings.brand} Documentation`);
+		} else if (get.page) {
+			// automatically catch any pages without dedicated routes
+			const cleanPage = utils.cleanString(get.page);
+			const cleanTitle = utils.titleCase(cleanPage);
+			await utils.loadModule(`pages/${cleanPage}.html`,'content');
+			utils.setTitle(`${vSettings.brand} | ${cleanTitle}`);
+		} else {
+			// load home page if no page= variable specified in URL
+			await utils.loadModule('pages/home.html','content');
+			utils.setTitle(`${vSettings.brand}`);
+			utils.setDescription(`ProgUX is the best library to make web faster`);
+		}
+		utils.scrollTo();
+		return true;
+	},
+}
