db2ac5d86855e9b08c2d4c191b9df28030fb18b1
Scott Francis <scottfrancisfrancis@hotmail.com>
2021-02-04 07:35:06 +0100

    NCB-3089 Demo site header/menu component Added a new demo layout file and configured demo index to use it Added a header component, njk and scss Added a menu component, njk and scss Added logo

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,6 +1,9 @@
 // Import SCSS
 import './demo.scss';
 import './components/stage/stage.scss';
+import './components/header/header.scss';
+import './components/menu/menu.scss';
 
 // Import JS
 import './components/stage/stage.js';
+import './components/header/header.js'
