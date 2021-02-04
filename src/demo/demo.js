bde4867fa7fa1ef82367636d79793776edf04a7b
Scott Francis <scottfrancisfrancis@hotmail.com>
2021-02-04 15:24:06 +0100

    NCB-3092 Demo site parallax component Added parallax component, CSS and JS

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -3,6 +3,8 @@ import './demo.scss';
 import './components/stage/stage.scss';
 import './components/header/header.scss';
 import './components/menu/menu.scss';
+import './components/parallax/parallax.scss';
 
 // Import JS
 import './components/stage/stage.js';
+import './components/parallax/parallax';
