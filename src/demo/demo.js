08bb92d4a4ca227dba723359b613a9f4b182bf92
Scott Francis <scottfrancisfrancis@hotmail.com>
2021-02-05 11:51:03 +0100

    NCB-3094 Merged develop Added classes to hide parallax Added script to load parallax image if progUX params allow

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -12,4 +12,25 @@ import './components/parallax/parallax';
 
 window.addEventListener('load', () => {
     document.querySelector('body').classList.add('loaded');
+
+    const progUXClasses = Array.from(document.getElementsByTagName('html')[0].classList);
+    const parallaxBlockingClasses = ['reducedMotion-true', 'saveData-true', 'connectionSpeed-slow', 'cpuLevel-low', 'memoryLevel-low'];
+    let hasParallaxBlocker = false;
+
+    for (let i = 0; i < progUXClasses.length; i++) {
+      if (parallaxBlockingClasses.includes(progUXClasses[i])) {
+        hasParallaxBlocker = true;
+
+        break;
+      }
+    }
+
+    if (!hasParallaxBlocker) {
+      const parallax = document.querySelector('.parallax__image');
+      const parallaxSrc = parallax.dataset.src;
+
+      parallax.src = parallaxSrc;
+    }
+
+
 });
