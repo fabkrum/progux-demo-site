176d0ac9326e9de83873e33b7d795075b7e9ecfe
Scott Francis <scottfrancisfrancis@hotmail.com>
2021-02-05 08:39:14 +0100

    NCB-3093 Demo site text component

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -3,6 +3,7 @@ import './demo.scss';
 import './components/stage/stage.scss';
 import './components/header/header.scss';
 import './components/menu/menu.scss';
+import './components/textblock/textblock.scss';
 
 // Import JS
 import './components/stage/stage.js';
