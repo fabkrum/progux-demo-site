5c3a1f8ef46c79907cac18e8f11e4c6c32461873
Caroline Erize <caroline.erize@netcentric.biz>
2021-02-05 10:49:20 +0100

    NCB-3091 Demo site card & button components * Created cards file and styles * Added cards to index * Updated breakpoint * Created css variable for animations * Updated components with css var * Updated cta styles for library settings * Created intersection observer for sections to trigger animations

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,12 +1,31 @@
 // Import SCSS
 import './demo.scss';
 import './components/banner/banner.scss';
+import './components/cards/cards.scss';
 import './components/cta/cta.scss';
 import './components/header/header.scss';
 import './components/menu/menu.scss';
 
 // Import JS
 
-window.addEventListener('load', () => {
-    document.querySelector('body').classList.add('loaded');
+const observerSettings = {
+    rootMargin: '0px',
+    threshold: 0.01
+};
+const sections = document.querySelectorAll('section');
+  
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            observer.unobserve(entry.target);
+
+            console.log(entry);
+            entry.target.classList.add('loaded');
+        }
+    })
+    
+}, observerSettings);
+
+sections.forEach(section => {
+    observer.observe(section);
 });
